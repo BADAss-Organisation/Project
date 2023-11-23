c0cabd4e5ff7bd9387346657fa3afd9b506f8222
Dimitar Dimitrov <109605825+dimitardimitrov05@users.noreply.github.com>
2023-11-23 10:12:42 +0200

    Delete BADAss_project directory

--- BADAss_project/Wanna Buyl/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Wanna_Buyl.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
