0bd46b6f4f541fee42116db1202beac6373a6bad
Aylinch <139501213+Aylinch@users.noreply.github.com>
2023-12-12 15:43:26 +0200

    navbar changes

---
+++ ProjectSE/WannaBuy/Controllers/ApplicationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WannaBuy.Data;
+
+namespace WannaBuy.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        public ApplicationController(ApplcationDbContext dbContext)
+        {
+            this.applicationDbContext = dbContext;
+        }
+       [HttpGet]
+       public async Task<IActionResult>Add()
+        {
+           
+        }
+    }
+}
---
+++ ProjectSE/WannaBuy/ViewModels/Application/AddApplicationViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WannaBuy.ViewModels.Application
+{
+    public class AddApplicationViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public byte[] Image { get; set; }
+        
+    }
+}
