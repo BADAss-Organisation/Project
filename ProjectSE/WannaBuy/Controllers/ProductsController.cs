1de26aa0d363d2d719f6f0b6705ed2b55943e1db
Aylinch <139501213+Aylinch@users.noreply.github.com>
2023-12-13 16:04:52 +0200

    navbar and product controller

---
+++ ProjectSE/WannaBuy/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WannaBuy.Data;
+using WannaBuy.Models.Entities;
+using WannaBuy.ViewModels.Application;
+
+namespace WannaBuy.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        public ProductsController(ApplicationDbContext dbContext)
+        {
+            this.applicationDbContext = dbContext;
+        }
+       [HttpGet]
+       public async Task<IActionResult> Add()
+        {
+            ViewBag.CategoryId = new SelectList(applicationDbContext.Categories, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]  
+        public async Task<IActionResult> Add(AddApplicationViewModel model)
+        {
+            var product = new Product()
+            {
+                Name = model.Name,
+                Price = model.Price,
+                Amount = model.Amount,
+                Description = model.Description,
+                Image = model.Image,
+            };
+            await applicationDbContext.AddAsync(product);
+            await applicationDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- ProjectSE/WannaBuy/ViewModels/Application/AddApplicationViewModel.cs
+++ ProjectSE/WannaBuy/ViewModels/Application/AddApplicationViewModel.cs
@@ -3,9 +3,11 @@
     public class AddApplicationViewModel
     {
         public string Name { get; set; }
+        public decimal Price { get; set; }  
         public string Description { get; set; }
         public decimal Amount { get; set; }
-        public byte[] Image { get; set; }
+        public string Image { get; set; }
+        public int CategoryId { get; set; }  
         
     }
 }
