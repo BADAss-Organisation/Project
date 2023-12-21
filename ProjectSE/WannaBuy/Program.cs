6bf8a20f7badceef21cded0af580d5dfffef70c8
Boris00534 <bohd110205@gmail.com>
2023-12-21 08:21:38 +0200

    .View for each application

--- ProjectSE/WannaBuy/Program.cs
+++ ProjectSE/WannaBuy/Program.cs
@@ -30,7 +30,7 @@ namespace WannaBuy
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for production scenarioss, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
